296ecd968160f14bf7cc8b5ae4c1ed5cd13594fc
Pramod <86370487+Pramod3245@users.noreply.github.com>
2024-05-17 23:03:54 +0530

    Update firebaseConfig.js
    took out important information

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -8,15 +8,7 @@ import {getFirestore, collection} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCfRG3D-mQG61q_yS1slm5CZ_l3itCR3-s",
-  authDomain: "ridem8app.firebaseapp.com",
-  projectId: "ridem8app",
-  storageBucket: "ridem8app.appspot.com",
-  messagingSenderId: "433877216520",
-  appId: "1:433877216520:web:33177159e0b6fb4bc490bd",
-  measurementId: "G-FVNCEDKGV3"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
